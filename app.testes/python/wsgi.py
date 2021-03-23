cea8ab49608ba0647849d4180e58343c6faf212e
amber-lab <leoamber27@gmail.com>
2021-03-23 16:34:14 +0000

    Testing

--- app.testes/python/wsgi.py
+++ app.testes/python/wsgi.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template, request, jsonify
+from flask import Flask, url_for, render_template, request, jsonify, make_response, redirect
 from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
@@ -15,13 +15,12 @@ def getStatic():
 	return url_for('static', filename='style.css')
 
 @app.route('/hello/')
-@app.route('/hello/<name>')
+@app.route('/hello/<string:name>')
 def hello(name=None):
 	return render_template('hello.html', name=name)
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-	error = None
 	print("\n", dir(request), "\n\n")
 	if request.method == 'POST':
 		input_json = request.get_json()
@@ -35,3 +34,25 @@ def upload_file():
 		file = request.files['file']
 		print("--------\n",secure_filename(file.filename),"\n--------")
 	return render_template('file_upload_template.html')
+
+@app.route('/cookietest')
+def cookietest():
+	req = request.cookies.get('username')
+	if req:
+		print("Has cookie")
+		return redirect('/hello/'+req)
+	else:
+		print("No cookie for you! :P")
+		return redirect('/hello')
+
+@app.route('/setcookie')
+@app.route('/setcookie/<string:usr>')
+def setcookie(usr=None):
+	print("\n\n",usr,"\n\n")
+	if usr and not request.cookies.get('username'):
+		resp = make_response(render_template('hello.html', name=usr))
+		resp.set_cookie('username', usr)
+		print("Cookie defined")
+		return resp
+	else:
+		return "No cookie to define or already defined"
